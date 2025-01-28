50f6664ce63d8a18d20c96a87db2a395ec05f28f
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2025-01-28 10:01:26 +0200

    New exercise

---
+++ 13.DOM-Events-Exercises/09-Encode-and-Decode-Messages/app.js
@@ -0,0 +1,27 @@
+function encodeAndDecodeMessages() {
+    const textAreas = document.querySelectorAll('textarea');
+    const buttons = document.querySelectorAll('button');
+    const [encodeBtn, decodeBtn] = buttons;
+    const [encodeTextArea, decodeTextArea] = textAreas;
+
+    encodeBtn.addEventListener('click', () => {
+        const message = encodeTextArea.value
+            .split('')
+            .map((char) => char.charCodeAt() + 1)
+            .map((charCode) => String.fromCharCode(charCode))
+            .join('');
+
+        decodeTextArea.value = message;
+        encodeTextArea.value = '';
+    });
+
+    decodeBtn.addEventListener('click', () => {
+        const message = decodeTextArea.value
+            .split('')
+            .map((char) => char.charCodeAt() - 1)
+            .map((charCode) => String.fromCharCode(charCode))
+            .join('');
+
+        decodeTextArea.value = message;
+    });
+}
