64916064f6c607eb6d60de67b1537596c27938ef
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2025-01-28 07:39:31 +0200

    Simple yet exercise

---
+++ 13.DOM-Events-Exercises/01-Subtraction/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+    let first = document.getElementById('firstNumber');
+    let second = document.getElementById('secondNumber');
+    
+    let result = Number(first.value) - Number(second.value);
+    
+    document.getElementById('result').textContent = result;
+} 
