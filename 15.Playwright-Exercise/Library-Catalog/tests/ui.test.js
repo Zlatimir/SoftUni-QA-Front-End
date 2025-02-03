266a9c4cc1585d3c9ce3c1d26611ef75e66e004f
ZlatimirIvanov <zlatimir_ivanov@abv.bg>
2025-02-03 17:24:48 +0200

    Tests added

--- 15.Playwright-Exercise/Library-Catalog/tests/ui.test.js
+++ 15.Playwright-Exercise/Library-Catalog/tests/ui.test.js
@@ -1,26 +1,69 @@
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Verify "All books" link is visible', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.waitForSelector('nav.navbar');
-    const allBooksLink = await page.$('nav.navbar a[href="/catalog"]');
-    const isLinkVisible = await allBooksLink.isVisible();
-    expect(isLinkVisible).toBe(true);
+test.describe('Navbar Links Visibility for non-logged user', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:3000');
+        await page.waitForSelector('nav.navbar');
+    });
+
+    test('Verify "All books" link is visible', async ({ page }) => {
+        const allBooksLink = await page.$('nav.navbar a[href="/catalog"]');
+        const isLinkVisible = await allBooksLink.isVisible();
+        expect(isLinkVisible).toBe(true);
+    });
+
+    test('Verify "Login" button is visible', async ({ page }) => {
+        const loginLink = await page.$('nav.navbar a[href="/login"]');
+        const isLinkVisible = await loginLink.isVisible();
+        expect(isLinkVisible).toBe(true);
+    });
+
+    test('Verify "Register" button is visible', async ({ page }) => {
+        const registerLink = await page.$('nav.navbar a[href="/register"]');
+        const isLinkVisible = await registerLink.isVisible();
+        expect(isLinkVisible).toBe(true);
+    });
 });
 
-test('Verify "Login" button is visible', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.waitForSelector('nav.navbar');
-    const allBooksLink = await page.$('nav.navbar a[href="/login"]');
-    const isLinkVisible = await allBooksLink.isVisible();
-    expect(isLinkVisible).toBe(true);
-});
+test.describe('Navbar Links Visibility for logged user', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:3000/login');
+        await page.fill('input[name="email"]', 'peter@abv.bg');
+        await page.fill('input[name="password"]', '123456');
+        await page.click('input[type="submit"]');
+    });
 
-test('Verify "Register" button is visible', async ({ page }) => {
-    await page.goto('http://localhost:3000');
-    await page.waitForSelector('nav.navbar');
-    const allBooksLink = await page.$('nav.navbar a[href="/register"]');
-    const isLinkVisible = await allBooksLink.isVisible();
-    expect(isLinkVisible).toBe(true);
-});
+    test.afterEach(async ({ page }) => {
+        await page.click('a[href="javascript:void(0)"]');
+    });
 
+    test('Verify "All books" link is visible', async ({ page }) => {
+        const allBooksLink = await page.$('a[href="/catalog"]');
+        const isLinkVisible = await allBooksLink.isVisible();
+        expect(isLinkVisible).toBe(true);
+    });
+
+    test('Verify "Logout" button is visible', async ({ page }) => {
+        const logoutLink = await page.$('a[href="javascript:void(0)"]');
+        const isLinkVisible = await logoutLink.isVisible();
+        expect(isLinkVisible).toBe(true);
+    });
+
+    test('Verify "My books" link is visible', async ({ page }) => {
+        const myBooksLink = await page.$('a[href="/profile"]');
+        const isLinkVisible = await myBooksLink.isVisible();
+        expect(isLinkVisible).toBe(true);
+    });
+
+    test('Verify "Add book" link is visible', async ({ page }) => {
+        const myBooksLink = await page.$('a[href="/create"]');
+        const isLinkVisible = await myBooksLink.isVisible();
+        expect(isLinkVisible).toBe(true);
+    });
+
+    test('Verify User email is visible', async ({ page }) => {
+        const welcomeSpan = await page.$('div#user span');
+        const textContent = await welcomeSpan.textContent();
+        expect(textContent).toContain('peter@abv.bg');
+    });
+});
